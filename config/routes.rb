63d717212abb53d2d50bc9f6d893f9a96199ffe7
Fred Argoud <fred.argoud@gmail.com>
2019-02-04 10:13:53 +0100

    side bar style

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'events#index'
 
   resources :events, only: [:index, :show] do
     resources :quotations, only: [:new, :create]
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,3 @@ puts 'Creating 5 products...'
   puts "#{i + 1}. #{event.name}"
 end
 puts 'Finished!'
-
-traiteur = Event.create!(name: 'Au Panier des Halles',
-                         description: "« Des produits frais et élégants, un savoir-faire élégant » \n Le traiteur Panier des Halles, vous livre sur Paris. Idéal pour les déjeuners d'entreprises, vous pouvez commander des plateaux repas, des menus cocktails ainsi que des menus végétariens. Organisation de buffets et cocktails pour les entreprises. Le partenaire de vos réceptions et de vos livraisons dans la ville de Fresnes, organise vos réceptions avec professionnalisme et créativité.\n
-Au panier des Halles est un réel partenaire de vos événements et mettra tout son savoir-faire à votre disposition afin de faire de votre réception une véritable réussite.',photo_url: 'https://photos.idealmeetingsevents.fr/20531/x1.jpg,q1483025051.pagespeed.ic.RmdJWeuhMW.webp",
-                         photo_url: 'https://photos.idealmeetingsevents.fr/20531/x1.jpg,q1483025051.pagespeed.ic.RmdJWeuhMW.webp')
