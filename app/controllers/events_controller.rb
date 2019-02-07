015bc92a08f596fb5fed2ea146b2ade582c88841
Fred Argoud <fred.argoud@gmail.com>
2019-02-07 11:37:27 +0100

    mapbox markers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,35 +5,11 @@ class EventsController < ApplicationController
     else
       @events = Event.all
     end
-
-    # @events = Event.where.not(latitude: nil, longitude: nil)
-
-    # @markers = @events.map do |event|
-    #   {
-    #     lng: event.longitude,
-    #     lat: event.latitude
-    #   }
-    # end
   end
 
   def show
+    @events = Event.where.not(latitude: nil, longitude: nil)
     @event = Event.find(params[:id])
-  end
-
-  def create
-    require "open-uri"
-    require "nokogiri"
-    html = open("https://www.1001traiteurs.com/lieu/france/auvergne-rhone-alpes/isere/grenoble").read
-    # 1. Parse HTML
-    doc = Nokogiri::HTML(html, nil, "utf-8")
-    # 2. For the first five results
-    @results = []
-    doc.search(".teaser-card").first(9).each do |element|
-      # 3. Create recipe and store it in results
-      name = element.search('.teaser-textblock>a').text.strip
-      description = element.search('.teaser-textblock>p').text.strip
-      @results << Event.new(name: name, description: description)
-    end
-    return @results
+    @markers = { lng: @event.longitude, lat: @event.latitude }
   end
 end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,38 +1,6 @@
-mport mapboxgl from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl';
 
-const initMapbox = () => {
-  const mapElement = document.getElementById('map');
-
-  if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
-  }
-};
-
-if (mapElement) {
-  // [ ... ]
-  const markers = JSON.parse(mapElement.dataset.markers);
-  markers.forEach((marker) => {
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .addTo(map);
-  });
-}
-
-
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-};
-
-if (mapElement) {
-  // [ ... ]
-  fitMapToMarkers(map, markers);
-}
+const mapElement = document.getElementById('map');
 
 const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -43,16 +11,16 @@ const buildMap = () => {
 };
 
 const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
+  // markers.forEach((marker) => {
     new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
+      .setLngLat([ markers.lng, markers.lat ])
       .addTo(map);
-  });
+  // });
 };
 
-const fitMapToMarkers = (map, markers) => {
+const fitMapToMarkers = (map, marker) => {
   const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  bounds.extend([ marker.lng, marker.lat ]);
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
 
@@ -64,6 +32,5 @@ const initMapbox = () => {
     fitMapToMarkers(map, markers);
   }
 };
-
 
 export { initMapbox };
