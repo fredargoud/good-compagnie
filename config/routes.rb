b61e07c12144af275efd8eeeeb9fa2d6f8391843
Fred Argoud <fred.argoud@gmail.com>
2019-02-14 17:04:18 +0100

    Add category to events

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   resources :events, only: [:index, :show] do
     resources :quotations, only: [:new, :create]
+
+    collection do                       # collection => no restaurant id in URL
+      get 'traiteurs'                         # RestaurantsController#top
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_28_140032) do
+ActiveRecord::Schema.define(version: 2019_02_14_160255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_01_28_140032) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.string "category"
   end
 
   create_table "quotations", force: :cascade do |t|
