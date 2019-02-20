f483838c7070d6ae518ba4d55c53bc4c35496fda
Fred Argoud <fred.argoud@gmail.com>
2019-02-20 10:38:14 +0100

    Decide to use a grid to display picture

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,9 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import flatpickr from "flatpickr";
 import { France } from "flatpickr/dist/l10n/fr.js";
+
 import '../plugins/date';
+import '../components/carousel';
 
 
 // internal imports
